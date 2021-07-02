ef183b80e9a7c5cddcac26e89323a5782f623d1b
isaias-silva <zackblackmangold@gmail.com>
2021-07-01 22:18:54 -0300

    hamburguer mobiloe finalizado

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const mmobile = window.document.getElementById('mobile');
+const menu_princi = window.document.getElementById('menucentral');
+mmobile.addEventListener("click", check)
+
+function check() {
+
+    menu_princi.classList.toggle('active');
+
+
+
+
+
+}
